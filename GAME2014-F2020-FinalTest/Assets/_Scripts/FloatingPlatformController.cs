e5d7c2e218fba8a55427c591ee38cf6f24c02a73
RavenBP <ravenbpowless@gmail.com>
2020-12-15 13:12:49 -0500

    Added project with floating platforms

---
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/FloatingPlatformController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPlatformController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Platform;
+
+    private BoxCollider2D boxCollider2D;
+    private bool triggered = false;
+    private bool shrunk = false;
+    private float amount = 0.001f;
+    private float scale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered == true)
+        {
+            transform.localScale = new Vector3(scale -= amount, scale -= amount, 1.0f);
+
+            if (scale <= 0.0f)
+            {
+                triggered = false;
+                shrunk = true;
+                Debug.Log("Platform reached smallest value");
+            }
+        }
+        else if (shrunk == true)
+        {
+            transform.localScale = new Vector3(scale += amount, scale += amount, 1.0f);
+
+            if (scale >= 1.0f)
+            {
+                shrunk = false;
+                Debug.Log("Platform reached largest value");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            triggered = true;
+        }
+    }
+}
